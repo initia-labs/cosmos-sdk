9fd233bcf8475f1af4790d64074a851d9a74b1c4
beer-1 <beer-1@codingcats.xyz>
2025-04-16 02:22:59 +0900

    fix incorrect retention height when commitHeight equals minRetainBlocks

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -1276,7 +1276,7 @@ func (app *BaseApp) CreateQueryContext(height int64, prove bool) (sdk.Context, e
 // need historical blocks.
 func (app *BaseApp) GetBlockRetentionHeight(commitHeight int64) int64 {
 	// pruning is disabled if minRetainBlocks is zero
-	if app.minRetainBlocks == 0 {
+	if app.minRetainBlocks == 0 || app.minRetainBlocks >= uint64(commitHeight) {
 		return 0
 	}
 
